f626a76b56147a2dee71af4346f10f3b2dea03dc
jamescardona11 <jjcardonao@gmail.com>
2022-05-11 19:32:29 -0500

    const vs let

---
+++ fh-react-course/02-intro-javascript/src/01-const-let.js
@@ -0,0 +1,14 @@
+console.log('Hola Mundo')
+
+// variables vs constantes
+// No usar `var`
+
+const myName = 'James';
+const surname = 'Cardona';
+
+let value = 5;
+value = 29;
+
+console.log(myName, surname, value);
+
+// Mismo funcionamiento en scopes - nada nuevo
